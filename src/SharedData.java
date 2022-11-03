27ef26ef76aa38b0fbbce43bc6fe5efd0b80fa13
Ben04 <user@10.10.12.119>
2022-11-03 18:16:19 +0200

    Final Update

--- src/SharedData.java
+++ src/SharedData.java
@@ -1,41 +1,66 @@
-public class SharedData 
-{
-	private int [] array;
-	private boolean [] winArray;
+import java.util.ArrayList;
+
+/**
+ * @author Daniel and Ben	
+ * Class SharedData This class shared the data with the threads
+ * 
+ */
+public class SharedData {
+	private ArrayList<Integer> array;
+	private boolean[] winArray;
 	private boolean flag;
 	private final int b;
-	
-	public SharedData(int[] array, int b) {
-		
+
+	/**
+	 * @param array
+	 * @param b
+	 * constructor that get array and integer b
+	 */
+	public SharedData(ArrayList<Integer> array, int b) {
 		this.array = array;
 		this.b = b;
 	}
 
-	public boolean[] getWinArray() 
-	{
+	/**
+	 * @return
+	 * return the array that win
+	 */
+	public boolean[] getWinArray() {
 		return winArray;
 	}
 
-	public void setWinArray(boolean [] winArray) 
-	{
+	/**
+	 * @param winArray
+	 * set the array that win
+	 */
+	public void setWinArray(boolean[] winArray) {
 		this.winArray = winArray;
 	}
 
-	public int[] getArray() 
-	{
+	/**
+	 * @return
+	 * return the array
+	 */
+	public ArrayList<Integer> getArray() {
 		return array;
 	}
 
-	public int getB() 
-	{
+	/**
+	 * @return
+	 * return b
+	 */
+	public int getB() {
 		return b;
 	}
 
-	public boolean getFlag() 
-	{
+	public boolean getFlag() {
 		return flag;
 	}
 
+	/**
+	 * @param flag
+	 * set the flag
+	 */
 	public void setFlag(boolean flag) {
 		this.flag = flag;
 	}
--- src/ThreadCheckArray.java
+++ src/ThreadCheckArray.java
@@ -1,100 +1,88 @@
+import java.util.ArrayList;
+
 /**
- * @author Daniel Maman and Ben
- * The program shows how, with the help of two threads,
- * it is possible to check whether there are elements in the array whose
- * sum is equal to a given sum
+ * @author Daniel Maman and Ben The program shows how, with the help of two
+ *         threads, it is possible to check whether there are elements in the
+ *         array whose sum is equal to a given sum
  */
-public class ThreadCheckArray implements Runnable 
-{
+public class ThreadCheckArray implements Runnable {
 	private boolean flag;
-	private boolean [] winArray;
+	private boolean[] winArray;
 	SharedData sd;
-	int[] array;
+	ArrayList<Integer> array;
 	int b;
-	
+
 	/**
-	 * @param sd - shared data
-	 * The function receives shared information and initializes it within a synchronized block
+	 * @param sd - shared data The function receives shared information and
+	 *           initializes it within a synchronized block
 	 */
-	public ThreadCheckArray(SharedData sd) 
-	{
-		this.sd = sd;	
-		synchronized (sd) 
-		{
+	public ThreadCheckArray(SharedData sd) {
+		this.sd = sd;
+		synchronized (sd) {
 			array = sd.getArray();
 			b = sd.getB();
-		}		
-		winArray = new boolean[array.length];
-	}
-	
-	/**
-	 * @param n
-	 * @param b
-	 * A function whose function is to search for the sum of numbers in an array
-	 *  and check whether their number is equal to a given sum
-	 */
-	void rec(int n, int b)
-	{
-		synchronized (sd) 
-		{
-			if (sd.getFlag())
-				return;
-		}	
-		if (n == 1)
-		{
-			if(b == 0 || b == array[n-1])
-			{
-				flag = true;
-				synchronized (sd) 
-				{
-					sd.setFlag(true);
-				}			
-			}
-			if (b == array[n-1])
-				winArray[n-1] = true;
-			return;
 		}
-		
-		rec(n-1, b - array[n-1]);
-		if (flag)
-			winArray[n-1] = true;
-		synchronized (sd) 
-		{
-			if (sd.getFlag())
-				return;
-		}	
-		rec(n-1, b);
+		winArray = new boolean[array.size()];
 	}
 
 	/**
-	 *RUN function which activates the thread and waits for the previous thread
-	 * to see if it has finished and if there is any point in continuing to search again.
-	 * 
+	 * @param n
+	 * @param b A function whose function is to search for the sum of numbers in an
+	 *          array and check whether their number is equal to a given sum
 	 */
-	public void run() {
-		if (array.length != 1)
-			if (Thread.currentThread().getName().equals("thread1"))
-				rec(array.length-1, b - array[array.length - 1]);
-			else 
-				rec(array.length-1, b);
-		if (array.length == 1)
-			if (b == array[0] && !flag)
-			{
-				winArray[0] = true;
+	void rec(int n, int b) {
+		synchronized (sd) {
+			if (sd.getFlag())
+				return;
+		}
+		if (n == 1) {
+			if (b == 0 || b == array.get(n - 1)) {
 				flag = true;
-				synchronized (sd) 
-				{
+				synchronized (sd) {
 					sd.setFlag(true);
 				}
 			}
+			if (b == array.get(n - 1))
+				winArray[n - 1] = true;
+			return;
+		}
+
+		rec(n - 1, b - array.get(n - 1));
 		if (flag)
-		{
+			winArray[n - 1] = true;
+		synchronized (sd) {
+			if (sd.getFlag())
+				return;
+		}
+		rec(n - 1, b);
+	}
+
+	/**
+	 * RUN function which activates the thread and waits for the previous thread to
+	 * see if it has finished and if there is any point in continuing to search
+	 * again.
+	 * 
+	 */
+	public void run() {
+		if (array.size() != 1)
 			if (Thread.currentThread().getName().equals("thread1"))
-				winArray[array.length - 1] = true;
-			synchronized (sd) 
-			{
+				rec(array.size() - 1, b - array.get(array.size() - 1));
+			else
+				rec(array.size() - 1, b);
+		if (array.size() == 1)
+			if (b == array.get(0) && !flag) {
+				winArray[0] = true;
+				flag = true;
+				synchronized (sd) {
+					sd.setFlag(true);
+				}
+			}
+		if (flag) {
+			if (Thread.currentThread().getName().equals("thread1"))
+				winArray[array.size() - 1] = true;
+			synchronized (sd) {
 				sd.setWinArray(winArray);
-			}	
+			}
 		}
 	}
 }
